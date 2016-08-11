43567eb01d9c0963fb0e71aee7b3f6b80f732d1a
Eddie Belter <ebelter@wustl.edu>
2016-08-11 15:24:21 -0500

    Allow adding of a yet undefined config key

--- lib/RefImp/Config.pm
+++ lib/RefImp/Config.pm
@@ -36,8 +36,7 @@ sub set {
     my ($key, $value) = @_;
     Carp::croak("No key/value to set config!") if not defined $key;
     Carp::croak("No value to set config!") if not defined $value; # must be at least defined ('') for now...
-    return $config->{$key} = $value if exists $config->{$key};
-    Carp::croak("Invalid key to set config! $key");
+    $config->{$key} = $value;
 }
 
 1;
