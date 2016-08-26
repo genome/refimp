8fee4cf839899fc70b173be48dadb5dc09cf6737
Eddie Belter <ebelter@wustl.edu>
2016-08-26 16:18:19 -0500

    Ace Seq: _init_ sets pad/unpad mappings

--- lib/RefImp/Ace/Sequence.pm
+++ lib/RefImp/Ace/Sequence.pm
@@ -8,6 +8,7 @@ sub new {
 
     my $self = bless \%params, $class;
     $self->_validate;
+    $self->_init_;
 
     $self;
 }
@@ -16,6 +17,36 @@ sub _validate {
     my $self = shift;
 
     die "No bases given!" if not $self->{bases};
+    $self->{pad_char} ||= '*';
+}
+
+sub _init_ {
+    my $self = shift;
+
+    my $bases = $self->{bases};
+	my $bases_length = length($bases);
+
+	my (@padded_to_unpadded, @unpadded_to_padded);
+	my $unpadded_index = 0;
+    my $pad_char = $self->{pad_char};
+
+	for ( my $padded_index = 0; $padded_index < $bases_length; $padded_index++ ) {
+		$unpadded_to_padded[$unpadded_index] = $padded_index;
+        if( substr($bases, $padded_index, 1) ne $pad_char ) {
+			$padded_to_unpadded[$padded_index] = $unpadded_index;
+			$unpadded_index++;
+		}
+		else {
+			$padded_to_unpadded[$padded_index] = $pad_char;
+		}
+	}
+
+    if ( $padded_to_unpadded[ $bases_length - 1 ] eq $pad_char ) { # remove trailing unpadded positions that are pads
+        pop @unpadded_to_padded;
+    }
+
+    $self->{padded_to_unpadded} = \@padded_to_unpadded;
+    $self->{unpadded_to_padded} = \@unpadded_to_padded;
 }
 
 1;
--- t/RefImp-Ace-Sequence.t
+++ t/RefImp-Ace-Sequence.t
@@ -17,13 +17,21 @@ subtest 'setup' => sub{
 
 };
 
+my $seq;
 subtest 'new' => sub{
-    plan tests => 2;
+    plan tests => 4;
 
     throws_ok(sub{ $pkg->new; }, qr/No bases given/, 'new fails w/o bases');
 
-    my $seq = $pkg->new(bases => "ATCG");
+    my $bases = "AA***TTT*CCC****GGGG**";
+    $seq = $pkg->new(bases => $bases);
     isa_ok($seq, $pkg);
+
+    my $end = length($bases) - 1;
+    my @expected_unpadded_to_padded = (qw/ 0 1 5 6 7 9 10 11 16 17 18 19 /);
+    is_deeply($seq->{unpadded_to_padded}, \@expected_unpadded_to_padded, 'unpadded_to_padded');
+    my @expected_padded_to_unpadded = (qw/ 0 1 * * * 2 3 4 * 5 6 7 * * * * 8 9 10 11 * * /);
+    is_deeply($seq->{padded_to_unpadded}, \@expected_padded_to_unpadded, 'padded_to_unpadded');
 
 };
 
