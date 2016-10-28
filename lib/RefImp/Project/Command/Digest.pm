7d161c82e32616d4744fd3de5a8a068bc176528d
Eddie Belter <ebelter@wustl.edu>
2016-10-28 18:46:23 -0500

    Project digest tree command

---
+++ lib/RefImp/Project/Command/Digest.pm
@@ -0,0 +1,12 @@
+package RefImp::Project::Command::Digest;
+
+use strict;
+use warnings;
+
+class RefImp::Project::Command::Digest {
+    is => 'Command::Tree',
+    doc => 'work with projects digests',
+};
+
+1;
+
