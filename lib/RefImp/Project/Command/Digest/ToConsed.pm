4580370feb9d6facb3af53c88918afe38851ef2e
Eddie Belter <ebelter@wustl.edu>
2016-11-01 14:05:09 -0500

    Fully complete digests to consed

--- lib/RefImp/Project/Command/Digest/ToConsed.pm
+++ lib/RefImp/Project/Command/Digest/ToConsed.pm
@@ -3,8 +3,8 @@ package RefImp::Project::Command::Digest::ToConsed;
 use strict;
 use warnings 'FATAL';
 
-use Cwd;
-use File::Basename;
+use File::Spec;
+use RefImp::Project::Command::Digest::SizesReader
 
 class RefImp::Project::Command::Digest::ToConsed {
     is => 'RefImp::Project::Command::Base',
@@ -22,73 +22,57 @@ sub execute {
     $self->status_message('Digest sizes to consed...');
 
     my $project = $self->project;
-    $self->status_message('Project: %s', $project->__dsiplay_name__);
+    $self->status_message('Project: %s', $project->__display_name__);
 
     my $edit_dir = $project->edit_directory;
     $self->fatal_message('No project edit_dir directory!') if not -d $edit_dir;
     my $digest_directory = $project->digest_directory;
     $self->fatal_message('No project diest directory!') if not -d $digest_directory;
 
-    my @sizes;
-    @sizes = chomp(@sizes=`ls *sizes| sort`);
-
     my $project_basename = RefImp::Project::Command::Digest->project_basename($project->name);
     $self->status_message('Project basename: %s', $project_basename);
 
-    foreach my $size_file (@sizes) {
-        my %enzyme_hash;
-        my %output_hash;
-        my ($sizes_date) = $size_file =~ /(\S+).sizes/;
+    my $dh = IO::Dir->new($digest_directory);
+    for (1..2) { $dh->read }
+    my @digests;
+    SIZES: while ( my $file_name = $dh->read ) {
+        next SIZES if $file_name !~ /\.sizes$/;
+        my $sizes_file = File::Spec->join($digest_directory, $file_name);
+        $self->status_message('Reading: %s', $sizes_file);
 
-        print "sizes_file: $size_file\n";
-
-        open SIZES, $size_file || die "couldn't open $size_file";
-
-        while( <SIZES> ) {
-            s/^\s+//g;
-            next if /^$/ ;
-            my ($clone_enzyme, $bands, $date)= split;
-
-            if ( $clone_enzyme=~ /$project_basename/ ) {
-                print "found $_";
-                my $szEnzymeName= &get_enzyme($clone_enzyme);
-                $enzyme_hash{$szEnzymeName}++;
-
-
-                $output_hash{$enzyme_hash{$szEnzymeName}}.= ">$szEnzymeName\n";
-
-                while( <SIZES> ) {
-                    $output_hash{$enzyme_hash{$szEnzymeName}}.= $_;
-                    chomp;
-                    last if ( $_ == -1 ) ;
-                }
-            }
+        my $reader = RefImp::Project::Command::Digest::SizesReader->new(file => $sizes_file);
+        DIGEST: while ( my $digest = $reader->next ) {
+            next DIGEST if $digest->{project_header} !~ /$project_basename/;
+            push @digests, $digest;
         }
+    }
+    $dh->close;
 
-        close SIZES;
+    $self->status_message('Digests found: %s', scalar(@digests));
 
-
-        foreach my $file_number (sort {$a <=> $b} keys %output_hash){
-            my $consed_out;
-            my $suffix= $file_number - 1;
-            $consed_out= ($file_number == 1)?
-            "$edit_dir/fragSizes" . $sizes_date. ".txt":
-            "$edit_dir/fragSizes" . $sizes_date . "_$suffix.txt";
-            print "$consed_out\n";
-
-            open CONSED, ">$consed_out" || die "couldn't open $consed_out for writing";
-            print CONSED $output_hash{$file_number};
-            close CONSED;
-        }
-
+    my $frag_sizes_file_template = File::Spec->join($project->edit_directory, 'fragSizes%s.txt');
+    my @frag_sizes_files;
+    foreach my $digest ( @digests ) {
+        my $enzyme_code = $digest->{project_header};
+        $enzyme_code =~ s/$project_basename//;
+        my $enzyme = RefImp::Project::Command::Digest->enzyme_for_code($enzyme_code);
+        $self->fatal_message('Failed to get enzyme for %s', $digest->{project_header}) if not $enzyme;
+        my $frag_sizes_file = sprintf($frag_sizes_file_template, $digest->{date});
+        push @frag_sizes_files, $frag_sizes_file;
+        $self->status_message('Writing %s digest to %s', $enzyme, $frag_sizes_file);
+        my $fh = IO::File->new($frag_sizes_file, 'a');
+        $fh->print(">$enzyme\n");
+        $fh->print( join("\n", @{$digest->{bands}}, '') );
+        $fh->close;
     }
 
-    chdir ($edit_dir) or warn "can't cd $edit_dir\n";
-    unlink "fragSizes.txt" if (-l "fragSizes.txt");
-    my $most_recent = `ls -t fragSizes*|head -1`;
-    chomp $most_recent;
-    system "ln -s $most_recent fragSizes.txt";
+    my $main_frag_sizes_file = sprintf($frag_sizes_file_template, '');
+    unlink $main_frag_sizes_file if -e $main_frag_sizes_file;
+    my ($most_recent_frag_sizes_file) = sort { $b cmp $a } @frag_sizes_files;
+    $self->status_message('Linking fragSizes.txt to %s', $most_recent_frag_sizes_file);
+    symlink $most_recent_frag_sizes_file, $main_frag_sizes_file;
 
+    $self->status_message('Digest sizes to consed...OK');
     return 1
 }
 
