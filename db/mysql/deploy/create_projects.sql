ce3b3de7e2808dcf769bbdd3ff014e21a85a66ad
Eddie Belter <ebelter@gmail.com>
2016-11-18 19:07:33 -0600

    Sqitch create projects

---
+++ db/mysql/deploy/create_projects.sql
@@ -0,0 +1,16 @@
+-- Deploy refimp:create_projects to mysql
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS projects (
+        id VARCHAR(32),
+	name VARCHAR(64),
+	directory VARCHAR(128),
+	status VARCHAR(32),
+	clone_type VARCHAR(32),
+
+	CONSTRAINT projects_id_pk PRIMARY KEY(id),
+	UNIQUE(name)
+);
+
+COMMIT;
