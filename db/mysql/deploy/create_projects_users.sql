28a69fb7cf941962cf11e1b38106ad026309acae
Eddie Belter <ebelter@gmail.com>
2016-11-21 16:53:14 -0600

    Sqitch create projects users

---
+++ db/mysql/deploy/create_projects_users.sql
@@ -0,0 +1,16 @@
+-- Deploy refimp:create_projects_users to mysql
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS projects_users (
+        project_id VARCHAR(32),
+        user_id VARCHAR(32),
+	purpose VARCHAR(16),
+	claimed_on DATE,
+
+	CONSTRAINT prouse_project_fk FOREIGN KEY(project_id) REFERENCES projects(id),
+	CONSTRAINT prouse_user_fk FOREIGN KEY(user_id) REFERENCES users(id),
+	UNIQUE(project_id, user_id, purpose)
+);
+
+COMMIT;
