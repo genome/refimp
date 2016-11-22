6d5e9ce1a8b9692b1828a4e6527ae56111d1d5b7
Eddie Belter <ebelter@gmail.com>
2016-11-22 15:47:02 -0600

    Sqitch create taxons

---
+++ db/mysql/deploy/create_taxons.sql
@@ -0,0 +1,15 @@
+-- Deploy refimp:create_taxons to mysql
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS taxons (
+        id VARCHAR(32),
+	name VARCHAR(64),
+	species_name VARCHAR(64),
+	strain_name VARCHAR(64),
+
+	CONSTRAINT taxons_id_pk PRIMARY KEY(id),
+	UNIQUE(species_name, strain_name)
+);
+
+COMMIT;
