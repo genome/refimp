4bfec46c5e14f7706877f788827511a6c934e036
Eddie Belter <ebelter@wustl.edu>
2016-12-16 19:05:54 -0600

    Fix create projects users sql

--- db/mysql/deploy/create_projects_users.sql
+++ db/mysql/deploy/create_projects_users.sql
@@ -5,16 +5,16 @@
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS projects_users (
-        project_id VARCHAR(32),
-        user_id VARCHAR(32),
-	purpose VARCHAR(16),
-	claimed_on DATE,
+    project_id VARCHAR(32),
+    user_id VARCHAR(32),
+    purpose VARCHAR(16),
+    claimed_on DATE,
 
-	CONSTRAINT prouse_pk PRIMARY KEY (project_id, user_id, purpose)
-	CONSTRAINT prouse_project_fk FOREIGN KEY(project_id) REFERENCES projects(id),
-	INDEX(project_id),
-	CONSTRAINT prouse_user_fk FOREIGN KEY(user_id) REFERENCES users(id),
-	INDEX(user_id),
+    CONSTRAINT prouse_pk PRIMARY KEY (project_id, user_id, purpose),
+    CONSTRAINT prouse_project_fk FOREIGN KEY(project_id) REFERENCES projects(id),
+    INDEX(project_id),
+    CONSTRAINT prouse_user_fk FOREIGN KEY(user_id) REFERENCES users(id),
+    INDEX(user_id)
 );
 
 COMMIT;
