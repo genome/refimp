4aeca70e8340749852bd5716bdf113834f14377a
Eddie Belter <ebelter@gmail.com>
2017-04-03 20:03:59 -0500

    Taxonomy uses objects in id by

--- lib/RefImp/Project/Taxonomy.pm
+++ lib/RefImp/Project/Taxonomy.pm
@@ -6,12 +6,10 @@ use warnings 'FATAL';
 class RefImp::Project::Taxonomy {
     table_name => 'projects_taxa',
     id_by => {
-        project_id => { is => 'Text', },
-        taxon_id => { is => 'Text', },
+        project => { is => 'RefImp::Project', id_by => 'project_id', },
+        taxon => { is => 'RefImp::Taxon', id_by => 'taxon_id', constraint_name => 'protax_taxa_fk', },
     },
     has => {
-        project => { is => 'RefImp::Project', id_by => 'project_id', },
-        taxon => { is => 'RefImp::Taxon', id_by => 'taxon_id', },
         common_name => { via => 'taxon', to => 'name', },
         species_name => {via => 'taxon', to => 'species_name', },
         chromosome => { is => 'Text', },
