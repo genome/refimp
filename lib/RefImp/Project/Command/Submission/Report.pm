6ec80e127b880e4b81acee6878e4e91a083c483f
Eddie Belter <ebelter@gmail.com>
2017-04-10 18:44:53 -0500

    Submission general report

--- lib/RefImp/Project/Command/Submission/Report.pm
+++ lib/RefImp/Project/Command/Submission/Report.pm
@@ -3,6 +3,9 @@ package RefImp::Project::Command::Submission::Report;
 use strict;
 use warnings;
 
+use RefImp::Util::Tablizer;
+use YAML;
+
 class RefImp::Project::Command::Submission::Report { 
     is => 'Command::V2',
     has_input => {
@@ -13,6 +16,12 @@ class RefImp::Project::Command::Submission::Report {
             require_user_verify => 0,
             doc => 'Submissions to generate the report.',
         },
+        type => {
+            is => 'Text',
+            valid_values => [qw/ general /],
+            default_value => 'general',
+            doc => 'Type of report to display.',
+        },
     },
     doc => 'generate a report of submision records',
 };
@@ -21,12 +30,23 @@ sub help_detail { __PACKAGE__->__meta__->doc }
 
 sub execute {
     my $self = shift;
+    my $type = $self->type;
+    $self->$type;
+}
 
-    my @submissions = $self->submissions;
-    for my $submission ( @submissions ) {
-        print $submission->__display_name__."\n";
+sub general {
+    my $self = shift;
+
+    my @rows = [qw/ Project Finisher Date Size  /];
+    push @rows, [ map { '-' x length } @{$rows[0]} ];
+    my %metrics;
+    for my $submission ( $self->submissions ) {
+        $metrics{'Number of Projects'}++;
+        my $size = $submission->project_size || 0;
+        $metrics{'Total Size'} = $size;
+        push @rows, [ $submission->project->name, join(',', map { $_->name } $submission->project->finishers), $submission->submitted_on, $size, ];
     }
-    1;
+    print RefImp::Util::Tablizer->format(\@rows).YAML::Dump(\%metrics);
 }
 
 1;
--- t/RefImp-Project-Command-Submission-Report.t
+++ t/RefImp-Project-Command-Submission-Report.t
@@ -16,19 +16,39 @@ subtest 'setup' => sub{
 
 
     my $project = RefImp::Project->get(1);
+    my $user = RefImp::User->get(1);
+    RefImp::Project::User->create(
+        project => $project,
+        user => $user,
+        purpose => 'finisher',
+    );
     push @{$setup{submissions}}, RefImp::Project::Submission->create(
         project => $project,
         directory => '/tmp',
         submitted_on => '2017-01-01',
+        project_size => 101000,
     );
     
 };
 
-subtest 'execute' => sub {
-    plan tests => 1;
+subtest 'general' => sub {
+    plan tests => 2;
 
+    my $output;
+    open local( *STDOUT), '>', \$output or die $!;
     my $report = $setup{pkg}->execute(submissions => $setup{submissions});
     ok($report->result, 'execute');
+
+
+    my $expected_output = <<OUT;
+Project       Finisher Date       Size  
+-------       -------- ----       ----  
+HMPB-AAD13A05 bobama   2017-01-01 101000
+---
+Number of Projects: 1
+Total Size: 101000
+OUT
+    is($output, $expected_output, 'output is correct');
 
 };
 
