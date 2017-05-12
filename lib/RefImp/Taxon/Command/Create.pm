ac2a81c2b398723c26ad1f19a420086922dc9be1
Eddie Belter <ebelter@gmail.com>
2017-05-12 17:10:48 -0500

    Grep not next in getting params

--- lib/RefImp/Taxon/Command/Create.pm
+++ lib/RefImp/Taxon/Command/Create.pm
@@ -34,7 +34,7 @@ sub execute {
     my $taxon = RefImp::Taxon->get(name => $name);
     $self->fatal_message('Found existing taxon: %s', $taxon->__display_name__) if $taxon;
 
-    my %params = map { next if not $self->$_; $_ => lc($self->$_) } (qw/ name species_name strain_name /);
+    my %params = map { $_ => lc($self->$_) } grep { defined $self->$_ } (qw/ name species_name strain_name /);
     $self->status_message("Taxon params:\n%s---\n", YAML::Dump(\%params));
     $taxon = RefImp::Taxon->create(%params);
     $self->fatal_message('Failed to create taxon!') if !$taxon;
