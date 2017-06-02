ccf080f4945522d452251cfae7a1e5a69acd436b
Eddie Belter <ebelter@gmail.com>
2017-06-02 17:09:27 -0500

    10X longranger command

---
+++ lib/RefImp/Tenx/Command/Longranger.pm
@@ -0,0 +1,12 @@
+package RefImp::Tenx::Command::Longranger;
+
+use strict;
+use warnings;
+
+class RefImp::Tenx::Command::Longranger {
+    is => 'Command::Tree',
+    doc => '10X Genomics longranger helpers',
+};
+
+1;
+
