fc40146b2f9d786695f584481928490a0936f44d
Eddie Belter <ebelter@gmail.com>
2017-06-05 16:50:04 -0500

    LR Status
    convert from command to module
    add help
    log check, then journal only if running

--- lib/RefImp/Tenx/Command/Longranger/Status.pm
+++ lib/RefImp/Tenx/Command/Longranger/Status.pm
@@ -1,23 +1,20 @@
-#!/usr/bin/env perl5.12.1
+package RefImp::Tenx::Command::Longranger::Status;
 
 use strict;
 use warnings 'FATAL';
 
-package RefImp::Tenx::Longranger::Status;
-
-use UR;
-
 use Date::Format 'time2str';
 use File::stat;
 use IPC::Open3;
+use List::MoreUtils;
 use Path::Class;
 
-class RefImp::Tenx::Longranger::Status {
+class RefImp::Tenx::Command::Longranger::Status {
     is => 'Command::V2',
     has_input => {
         directory => {
             is => 'Text',
-            default_value => '/gscmnt/gc2745/graveslab/alignments/MMY_H_QD-WUPAT005-V0DHN2_with_pippen_size_selection-targeted-lsf',
+            #default_value => '/gscmnt/gc2745/graveslab/alignments/MMY_H_QD-WUPAT005-V0DHN2_with_pippen_size_selection-targeted-lsf',
         },
     },
     has_optional_input => {
@@ -30,22 +27,45 @@ class RefImp::Tenx::Longranger::Status {
         _directory => { calculate_from => [qw/ directory /], calculate => q| Path::Class::dir($directory) |, },
     },
     has_constant => {
-        datetime_format => { default_value => '%Y-%m-%d %H:%M:%S', },
-        now => { default_value => time(), },
+        datetime_format => { value => '%Y-%m-%d %H:%M:%S', },
+        now => { value => time(), },
     },
-    has_optional_transient => {
-        journal_status => { is => 'Text', },
-    },
-    doc => '',
+    doc => 'determine the status of a longranger run',
 };
+
+sub help_detail {
+    return <<HELP;
+
+Checks log and journal and tries to determine the longranger run status.
+
+Log File
+
+The tail of the log file is inspected looking for 2 known strings that indicate
+if the run has completed successfully. If "Pipestance completed successfully" is
+found, then the run is SUCCEEDED. If "Pipestance failed" is found, then the run
+tatus is FAILED. If niether of these is found, the log status will be RUNNING.
+The log access time will be reported, but the log may go a long time between
+writes. If the log status is not RUNNING, they journal status will ot be reported.
+
+Journal Directory
+
+The journal directrory  is often accessed throughoutthe run and is a good
+indicator of the longranger run status. Journal acceess time doesn't typically
+lag more than a couple of minutes. The time since last access threshold is 10
+miunutes. If the journal has been accessed within 10 minutes, the status is PASS,
+other wise it is FAIL.
+
+HELP
+}
 
 sub execute {
     my $self = shift;
 
     $self->status_message('Longranger Run Status...');
+    $self->status_message('Directory:        %s', $self->directory);
     $self->status_message('Current time:     %s', time2str($self->datetime_format, $self->now));
-    $self->_journal;
-    $self->_log;
+    my $log_status = $self->_log;
+    $self->_journal if $log_status eq 'running';
 
     1;
 }
@@ -58,8 +78,8 @@ sub _journal {
     $self->status_message('Journal accessed: %s', time2str($self->datetime_format, $journal_st->mtime));
     my $journal_access_min = (($self->now - $journal_st->mtime) / 60);
     $self->status_message('Minutes since:    %.1f', $journal_access_min);
-    my $journal_status = ( $journal_access_min < 10 ? 'PASS' : 'FAIL' );
-    $self->status_message('Journal status    %s', $journal_status);
+    my $journal_status = ( $journal_access_min < 10 ? 'pass' : 'fail' );
+    $self->status_message('Journal status    %s', uc $journal_status);
     $self->journal_status($journal_status);
 }
 
@@ -70,15 +90,28 @@ sub _log {
     my $log_st = stat($log_file) or die "$!",
 
     my($wtr, $rdr, $err);
-    #use Symbol 'gensym'; $err = gensym;
     my $pid = open3($wtr, $rdr, $err, 'tail', $log_file->stringify);
     waitpid( $pid, 0);
-    while (<$rdr>) { print }
+    my @log_tail = <$rdr>;
+    my $status = 'running';
+    if ( List::MoreUtils::any { $_ =~ /Pipestance completed successfully/ } @log_tail ) {
+        $status = 'succeeded';
+    }
+    elsif ( List::MoreUtils::any { $_ =~ /Pipestance failed/ } @log_tail ) {
+        $status = 'failed';
+        my $error_file = dir($self->directory)->parent->file($log_tail[-2]);
+        print "$error_file\n";
+        if ( -e "$error_file" ) {
+            my $error_content = $error_file->slurp($error_file);
+            print "$error_content\n";
+        }
+        #H_QD-WAPAT022-V0DHO7_targetde/PHASER_SVCALLER_EXOME_CS/PHASER_SVCALLER_EXOME/_SNPINDEL_PHASER/_SNPINDEL_CALLER/POPULATE_INFO_FIELDS/fork0/chnk0/_errors
+    }
 
     $self->status_message('Log accessed:     %s', time2str($self->datetime_format, $log_st->mtime));
+    $self->status_message(join('', @log_tail)) if $self->show_log_tail;
+    $self->status_message('Log status:       %s', uc $status);
+    $status;
 }
 
-package main;
-
-RefImp::Tenx::Longranger::Status->execute_with_shell_params_and_exit;
 1;
