1b9c0fba97ae43f98da62c682d192b46366f16bb
Eddie Belter <ebelter@gmail.com>
2017-07-10 18:45:45 -0500

    TenX Reads
    mv samplehseet into reads
    split samplesheet into samplesheet and run

--- lib/RefImp/Tenx/Reads/MkfastqRun.pm
+++ lib/RefImp/Tenx/Reads/MkfastqRun.pm
@@ -1,4 +1,4 @@
-package RefImp::Tenx::SampleSheet;
+package RefImp::Tenx::Reads::MkfastqRun;
 
 use strict;
 use warnings;
@@ -9,10 +9,13 @@ use Path::Class;
 use Params::Validate qw/ :types validate_pos /;
 use Text::CSV;
 
-class RefImp::Tenx::SampleSheet { 
+class RefImp::Tenx::Reads::MkfastqRun { 
     has => {
         directory => { is => 'Path::Class::Dir', },
-        samples => { is => 'ARRAY', },
+        samplesheet => { is => 'RefImp::Tenx::Reads::SampleSheet', },
+        lanes => { via => 'samplesheet', to => 'lanes', },
+        samples => { via => 'samplesheet', to => 'samples', },
+        sample_names => { via => 'samplesheet', to => 'sample_names', },
     },
     has_optional => {
         project_name => { is => 'Text', },
@@ -20,46 +23,16 @@ class RefImp::Tenx::SampleSheet {
     doc => 'sample sheet for running mkfastq and creating reads db entries',
 };
 
-sub create_from_mkfastq_directory {
-    my ($class, $directory) = @_;
-
-    $class->fatal_message('No mkfastq directory given!') if not $directory;
-    $class->fatal_message('Mkfastq directory given does not exist: %s', $directory) if !-d $directory;
+sub create {
+    my ($class, $directory) = validate_pos(@_, {is => __PACKAGE__}, {is => SCALAR});
 
     $directory = dir($directory);
+    $class->fatal_message('Mkfastq directory given does not exist: %s', $directory) if !-d $directory->stringify;
+
     my $file = $directory->subdir('outs')->file('input_samplesheet.csv');
     $class->fatal_message('No samplesheet found in mkfastq directory: %s', $file) if !-s $file->stringify;
 
-    my $fh = IO::File->new($file, 'r');
-    $class->fatal_message('Failed to open %s => %s', $file, $!) if not $fh;
-
-    my $csv = Text::CSV->new({sep_char => ','});
-    $class->fatal_message('Failed to create Text::CSV object!') if not $csv;
-
-    my $column_names;
-    do {
-        $column_names = $csv->getline($fh);
-    } until not defined $column_names or List::MoreUtils::any { /^sample/i } @$column_names;
-    $class->fatal_message('No sample column found in %s', $file) if not $column_names or not @$column_names;
-    $csv->column_names(@$column_names);
-
-    my @samples;
-    while ( my $info = $csv->getline_hr($fh) ) {
-        my %sample = map { my $v = delete $info->{$_}; lc $_ => $v } keys %$info;
-        $class->fatal_message('No index found in %s', Data::Dumper::Dumper($info)) if not $sample{index};
-
-        my $sample_key = List::MoreUtils::firstval { exists $sample{$_} } (qw/ sample sample_id /);
-        $class->fatal_message('No sample key found in %s', Data::Dumper::Dumper($info)) if not $sample_key;
-        $sample{name} = delete $sample{$sample_key};
-        $class->fatal_message('No sample name found for %s in %s', $sample_key, Data::Dumper::Dumper($info)) if not $sample{name};
-
-        $class->fatal_message('No lane found for sample %s', Data::Dumper::Dumper($info)) if not $sample{lane};
-
-        my $project_key = List::MoreUtils::firstval { exists $sample{$_} } (qw/ project sample_project /);
-        $sample{project} = delete $sample{$project_key} if $project_key;
-
-        push @samples, \%sample;
-    }
+    my $samplesheet = RefImp::Tenx::Reads::SampleSheet->create($file);
 
     my $project_name;
     my $invocation_file = $directory->file('_invocation');
@@ -69,11 +42,11 @@ sub create_from_mkfastq_directory {
 
     my %params = (
         directory => $directory,
-        samples => \@samples,
+        samplesheet => $samplesheet,
     );
     $params{project_name} = $project_name if $project_name;
 
-    $class->create(%params);
+    $class->SUPER::create(%params);
 }
 
 sub get_project_from_invocation {
@@ -90,18 +63,6 @@ sub get_project_from_invocation {
     $fh->close;
 
     $project;
-}
-
-sub sample_names {
-    my $self = shift;
-    my @names = sort map { $_->{name} } @{$self->samples};
-    List::MoreUtils::uniq @names;
-}
-
-sub lanes {
-    my $self = shift;
-    my @lanes = sort map { $_->{lane} } @{$self->samples};
-    List::MoreUtils::uniq @lanes;
 }
 
 sub fastq_directory_for_sample_name {
