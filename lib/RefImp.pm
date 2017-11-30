0933e7bb495f9ba384e192339321710da70a6022
Eddie Belter <ebelter@gmail.com>
2017-11-30 21:53:58 +0000

    Remove carp overload

--- lib/RefImp.pm
+++ lib/RefImp.pm
@@ -1,12 +1,10 @@
 package RefImp;
 
-use warnings;
+use warnings 'FATAL';
 use strict;
 
 our $VERSION = '0.010100';
 
-use Carp;
-use Carp::Heavy;
 use RefImp::Config;
 use UR;
 
@@ -20,40 +18,4 @@ if ( $ENV{REFIMP_CONFIG_FILE} ) {
     RefImp::Config::load_config_from_file( $ENV{REFIMP_CONFIG_FILE} );
 }
 
-# Account for a perl bug in pre-5.10 by applying a runtime patch to Carp::Heavy
-if ($] < 5.01) {
-    no warnings;
-    *Carp::caller_info = sub {
-        package
-            Carp;
-        our $MaxArgNums;
-        my $i = shift(@_) + 1;
-        package DB;
-        my %call_info;
-        @call_info{
-            qw(pack file line sub has_args wantarray evaltext is_require)
-        } = caller($i);
-
-        unless (defined $call_info{pack}) {
-            return ();
-        }
-
-        my $sub_name = Carp::get_subname(\%call_info);
-        if ($call_info{has_args}) {
-            # SEE IF WE CAN GET AROUND THE BIZARRE ARRAY COPY ERROR...
-            my @args = ();
-            if ($MaxArgNums and @args > $MaxArgNums) { # More than we want to show?
-                $#args = $MaxArgNums;
-                push @args, '...';
-            }
-            # Push the args onto the subroutine
-            $sub_name .= '(' . join (', ', @args) . ')';
-        }
-        $call_info{sub_name} = $sub_name;
-        return wantarray() ? %call_info : \%call_info;
-    };
-    use warnings;
-}
-
 1;
-
