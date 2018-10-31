873dc89922747b65d6ea30bd0f9d41f59e3c482f
Eddie Belter <ebelter@gmail.com>
2018-10-31 17:36:47 -0500

    Tenx Assembly __errors__

--- lib/Tenx/Assembly.pm
+++ lib/Tenx/Assembly.pm
@@ -26,4 +26,22 @@ class Tenx::Assembly {
     data_source => Tenx::Config::get('tenx_ds'),
 };
 
+sub __errors__ {
+    my $self = shift;
+
+    my @errors = $self->SUPER::__errors__;
+    return @errors if @errors;
+
+    my @existing_assemblies = grep { $_->id ne $self->id } __PACKAGE__->get(url => $self->url);
+    push @errors, UR::Object::Tag->create(
+        type => 'error',
+        properties => [qw/ url /],
+        desc => sprintf('Found existing assembly with url: %s', join(',', map { $_->__display_name__} @existing_assemblies)),
+    ) if @existing_assemblies;
+
+    @errors;
+}
+
+sub mkoutput_types { (qw/ raw megabubbles psuedohap2 /) }
+
 1;
