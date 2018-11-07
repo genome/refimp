d25146f1b67c8395a9f2b9b93647216441aa6bfc
Eddie Belter <ebelter@gmail.com>
2018-11-07 12:40:54 -0600

    Alignments
    merge sqlite sqitch db

---
+++ db/sqlite/deploy/create_alignments.sql
@@ -0,0 +1,19 @@
+-- Deploy create_alignments to sqlite
+-- requires refseqs
+-- requires reads
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS alignments (
+        id VARCHAR(32),
+	url VARCHAR(256),
+	status VARCHAR(16),
+	reads_id VARCHAR(256),
+	refseq_id VARCHAR(32),
+
+	CONSTRAINT aln_pk PRIMARY KEY(id),
+	CONSTRAINT alnref_fk FOREIGN KEY(refseq_id) REFERENCES refseqs(id)
+	CONSTRAINT alnrds_fk FOREIGN KEY(reads_id) REFERENCES reads(id)
+);
+
+COMMIT;
