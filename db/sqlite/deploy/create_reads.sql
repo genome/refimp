f28671d24809b5859145039dbd399277849d32ce
Eddie Belter <ebelter@gmail.com>
2018-11-07 12:35:46 -0600

    DB Migration Reads
    rename tenx reads to reads

---
+++ db/sqlite/deploy/create_reads.sql
@@ -0,0 +1,14 @@
+-- Deploy create_reads to sqlite
+
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS reads (
+        id VARCHAR(32),
+	sample_name VARCHAR(64),
+	url VARCHAR(256),
+	targets_url VARCHAR(256),
+
+	CONSTRAINT reads_pk PRIMARY KEY(id)
+);
+
+COMMIT;
