e62e2f9c5d4c134ad06cf7f6bd8066285ef3bf59
Eddie Belter <ebelter@gmail.com>
2018-11-08 19:53:42 -0600

    Update test DB and data

--- t/data/test.data.sql
+++ t/data/test.data.sql
@@ -9,6 +9,9 @@ INSERT OR IGNORE INTO taxa (id, name, species_name) VALUES ('2', 'eastern oyster
 
 INSERT OR IGNORE INTO users (id, name, first_name, last_name, email) VALUES ('1', 'bobama', 'barack', 'obama', 'obama@bestprezever.com');
 
-INSERT OR IGNORE INTO projects_taxa(project_id, taxon_id, chromosome) VALUES ('1', '1', '7');
+INSERT OR IGNORE INTO projects_taxa (project_id, taxon_id, chromosome) VALUES ('1', '1', '7');
+
+INSERT OR IGNORE INTO assemblies (id, name, tech) VALUES ('101', 'TENX-ASSEMBLY', 'tenx');
+INSERT OR IGNORE INTO assemblies (id, name, tech) VALUES ('102', 'PACBIO-ASSEMBLY', 'pacbio');
 
 COMMIT;
