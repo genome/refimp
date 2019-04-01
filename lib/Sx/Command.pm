26fe13fd1ced5b4fc7ad4d63ad7e3c50a614f508
Eddie Belter <ebelter@gmail.com>
2019-04-01 18:01:32 -0500

    SX rename command

--- lib/Sx/Command.pm
+++ lib/Sx/Command.pm
@@ -8,4 +8,12 @@ class Sx::Command {
     doc => 'Transform sequences with ease',
 };
 
+# This map allows the top-level commands to be set
+# instead of using the directory structure
+my %command_map = (
+    rename => 'Sx::Command::Rename',
+);
+
+$Sx::Command::SUB_COMMAND_MAPPING = \%command_map;
+
 1;
